624c07431c1de51c51ce878f0507100bd7adf0fd
Bertrand Dunogier <bd@ez.no>
2014-12-08 18:52:36 +0100

    EZP-23763: recommendation config doc

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,11 +16,23 @@ class Configuration extends SiteAccessConfiguration
         $systemNode
             ->arrayNode( 'yoochoose' )
                 ->children()
-                    ->scalarNode( 'customer_id' )->isRequired()->end()
-                    ->scalarNode( 'license_key' )->isRequired()->end()
+                    ->scalarNode( 'customer_id' )
+                        ->info( "YooChoose customer ID" )
+                        ->example( "12345" )
+                        ->isRequired()
+                    ->end()
+                    ->scalarNode( 'license_key' )
+                        ->info( "YooChoose license key" )
+                        ->example( "1234-5678-9012-3456-7890" )
+                        ->isRequired()
+                    ->end()
                 ->end()
             ->end()
-            ->scalarNode( 'server_uri' )->isRequired()->end();
+            ->scalarNode( 'server_uri' )
+                ->info( "HTTP base URI of the eZ Publish server" )
+                ->example( "http://site.com" )
+                ->isRequired()
+            ->end();
 
         return $treeBuilder;
     }

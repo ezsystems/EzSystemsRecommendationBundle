e402dd1bcd2ff59fc6b48b585ef3656451becc93
Bertrand Dunogier <bd@ez.no>
2014-12-22 16:09:54 +0100

    phpdoc

--- EzSystemsRecommendationBundle.php
+++ EzSystemsRecommendationBundle.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * This file is part of the EzSystemRecommendationBundle package
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributd with this source code.
+ */
 
 namespace EzSystems\RecommendationBundle;
 
--- eZ/Publish/LegacySearch/ConfigurationMapper.php
+++ eZ/Publish/LegacySearch/ConfigurationMapper.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the eZ Publish Kernel package
+ * This file is part of the EzSystemsRecommendationBundle package
  *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributd with this source code.

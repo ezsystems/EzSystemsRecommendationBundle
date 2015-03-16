f835276edb32756535d6cd6d9143da7756b7de54
Rafał Toborek <rafal.toborek@ez.no>
2015-03-16 15:10:55 +0100

    Added LocationHelper as a service

--- Controller/RecommendationController.php
+++ Controller/RecommendationController.php
@@ -9,43 +9,20 @@
 
 namespace EzSystems\RecommendationBundle\Controller;
 
-use eZ\Bundle\EzPublishCoreBundle\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-
 
 class RecommendationController
 {
-    protected $recommender, $repository, $router, $criteriaHelper;
+    protected $recommender, $repository, $router, $criteriaHelper, $locationHelper;
 
-    public function __construct( $recommender, $repository, $router, $criteriaHelper )
+    public function __construct( $recommender, $repository, $router, $criteriaHelper, $locationHelper )
     {
         $this->recommender = $recommender;
         $this->repository = $repository;
         $this->router = $router;
         $this->criteriaHelper = $criteriaHelper;
-    }
-
-    /**
-     * Transform location object into array
-     *
-     * @param \eZ\Publish\API\Repository\Values\Content\Location $location
-     * @param string $language
-     * @return array
-     */
-    private function mapLocationToArray( $location, $language )
-    {
-        $contentData = $this->repository->getContentService()->loadContentByContentInfo( $location->valueObject->contentInfo );
-
-        return array(
-            'name' => $location->valueObject->contentInfo->name,
-            'url' => $this->router->generate( $location->valueObject, array(), UrlGeneratorInterface::ABSOLUTE_PATH ),
-            'image' => $contentData->getFieldValue( 'image', $language )->uri,
-            'intro' => $contentData->getFieldValue( 'intro', $language )->xml->textContent,
-            'timestamp' => $contentData->getVersionInfo()->creationDate->getTimestamp(),
-            'author' => (string) $contentData->getFieldValue( 'author', $language )
-        );
+        $this->locationHelper = $locationHelper;
     }
 
     public function recommendationsAction( Request $request )
@@ -84,7 +61,7 @@ class RecommendationController
 
             foreach ($searchResults->searchHits as $result)
             {
-                $content[] = $this->mapLocationToArray( $result, $language );
+                $content[] = $this->locationHelper->mapLocationToArray( $result, $language );
             }
         }
 
---
+++ Helper/LocationHelper.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * File containing the LocationHelper class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace EzSystems\RecommendationBundle\Helper;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Helper class for building criteria easily.
+ */
+class LocationHelper
+{
+    protected $repository, $router;
+
+    public function __construct( $repository, $router )
+    {
+        $this->repository = $repository;
+        $this->router = $router;
+    }
+
+    /**
+     * Transform location object into array
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Location $location
+     * @param string $language
+     * @return array
+     */
+    public function mapLocationToArray( $location, $language )
+    {
+        $contentData = $this->repository->getContentService()->loadContentByContentInfo( $location->valueObject->contentInfo );
+
+        return array(
+            'name' => $location->valueObject->contentInfo->name,
+            'url' => $this->router->generate( $location->valueObject, array(), UrlGeneratorInterface::ABSOLUTE_PATH ),
+            'image' => $contentData->getFieldValue( 'image', $language )->uri,
+            'intro' => $contentData->getFieldValue( 'intro', $language )->xml->textContent,
+            'timestamp' => $contentData->getVersionInfo()->creationDate->getTimestamp(),
+            'author' => (string) $contentData->getFieldValue( 'author', $language )
+        );
+    }
+}

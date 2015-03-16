01b0dc5c9db30d8e0d5109d39202df3571fc6b8f
Rafał Toborek <rafal.toborek@ez.no>
2015-03-16 14:33:34 +0100

    Removed DOMContentLoaded from handlebarshelpers.js - not needed anymore

--- Resources/public/js/handlebarshelpers.js
+++ Resources/public/js/handlebarshelpers.js
@@ -1,15 +1,11 @@
 // register HandleBars helpers when library is loaded
 
-document.addEventListener('DOMContentLoaded', function () {
-    Handlebars.registerHelper('formatDate', function (timestamp) {
-        var t = new Date(parseInt(timestamp, 10) * 1000);
-        return t.toLocaleDateString();
-    });
-
-    Handlebars.registerHelper('formatTime', function (timestamp) {
-        var t = new Date(parseInt(timestamp, 10) * 1000);
-        return t.toLocaleTimeString();
-    });
+Handlebars.registerHelper('formatDate', function (timestamp) {
+    var t = new Date(parseInt(timestamp, 10) * 1000);
+    return t.toLocaleDateString();
 });
 
-
+Handlebars.registerHelper('formatTime', function (timestamp) {
+    var t = new Date(parseInt(timestamp, 10) * 1000);
+    return t.toLocaleTimeString();
+});

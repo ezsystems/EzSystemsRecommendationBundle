b06ae9abdcf12ed799cda3296af85eb1e8b3bb5a
Damian Zabawa <damian.zabawa@ez.no>
2018-01-14 14:57:20 +0100

    added tests

---
+++ Tests/Helper/FileSystemTest.php
@@ -0,0 +1,300 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\RecommendationBundle\Tests\Helper;
+
+use EzSystems\RecommendationBundle\Helper\FileSystem;
+use PHPUnit_Framework_TestCase;
+
+class FileSystemTest extends PHPUnit_Framework_TestCase
+{
+    /** @var \PHPUnit_Framework_MockObject_MockObject|\Symfony\Component\Filesystem\Filesystem */
+    private $baseFileSystem;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->baseFileSystem = $this->getMockBuilder('Symfony\Component\Filesystem\Filesystem')->getMock();
+    }
+
+    public function testLoad()
+    {
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('exists')
+            ->withAnyParameters()
+            ->willReturn(true)
+        ;
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $result = $fileSystem->load('testfile.txt');
+
+        $this->assertContains('testfile.txt content', $result);
+    }
+
+    /**
+     * @expectedException \eZ\Publish\Core\REST\Common\Exceptions\NotFoundException
+     * @expectedExceptionMessage File not found.
+     */
+    public function testLoadUnexistingFile()
+    {
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('exists')
+            ->withAnyParameters()
+            ->willReturn(false)
+        ;
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $result = $fileSystem->load('unexisting_file.txt');
+    }
+
+    public function testSave()
+    {
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('dumpFile')
+            ->withAnyParameters()
+            ->willReturn(true)
+        ;
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $fileSystem->save('testfile.txt', 'test');
+    }
+
+    public function testGetDir()
+    {
+        $dir = 'directory/';
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            $dir
+        );
+
+        $result = $fileSystem->getDir();
+
+        $this->assertEquals($dir, $result);
+    }
+
+    public function testCreateChunkDir()
+    {
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('exists')
+            ->withAnyParameters()
+            ->willReturn(true)
+        ;
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $result = $fileSystem->createChunkDir();
+
+        $this->assertTrue(strlen($result) > 5);
+    }
+
+    public function testCreateChunkDirWithUnexistingDir()
+    {
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('exists')
+            ->withAnyParameters()
+            ->willReturn(false)
+        ;
+
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('mkdir')
+            ->withAnyParameters()
+            ->willReturn(true)
+        ;
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $result = $fileSystem->createChunkDir();
+
+        $this->assertTrue(strlen($result) > 5);
+    }
+
+    public function testLock()
+    {
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('touch')
+            ->withAnyParameters()
+            ->willReturn(true)
+        ;
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $fileSystem->lock();
+    }
+
+    public function testUnlock()
+    {
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('exists')
+            ->withAnyParameters()
+            ->willReturn(true)
+        ;
+
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('remove')
+            ->withAnyParameters()
+            ->willReturn(true)
+        ;
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $result = $fileSystem->unlock();
+    }
+
+    public function testUnlockWithoutLockedFile()
+    {
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('exists')
+            ->withAnyParameters()
+            ->willReturn(false)
+        ;
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $result = $fileSystem->unlock();
+    }
+
+    public function testiIsLockedWithLockedFile()
+    {
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('exists')
+            ->withAnyParameters()
+            ->willReturn(true)
+        ;
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $this->assertTrue($fileSystem->isLocked());
+    }
+
+    public function testiIsLockedWithoutLockedFile()
+    {
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('exists')
+            ->withAnyParameters()
+            ->willReturn(false)
+        ;
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $this->assertFalse($fileSystem->isLocked());
+    }
+
+    public function testSecureDirWithMethodNone()
+    {
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $result = $fileSystem->secureDir(
+            'dir',
+            [
+                'method' => 'none',
+            ]
+        );
+
+        $this->assertEquals([], $result);
+    }
+
+    public function testSecureDirWithMethodUser()
+    {
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $result = $fileSystem->secureDir(
+            'dir',
+            [
+                'method' => 'user',
+                'login' => 'login',
+                'password' => 'pass',
+            ]
+        );
+
+        $this->assertEquals(
+            [
+                'login' => 'login',
+                'password' => 'pass',
+            ],
+            $result
+        );
+    }
+
+    public function testSecureDirWithMethodBasic()
+    {
+        $this->baseFileSystem
+            ->expects($this->once())
+            ->method('dumpFile')
+            ->withAnyParameters()
+            ->willReturn(true)
+        ;
+
+        $fileSystem = new FileSystem(
+            $this->baseFileSystem,
+            __DIR__ . '/../fixtures/'
+        );
+
+        $result = $fileSystem->secureDir(
+            'dir',
+            [
+                'method' => 'basic',
+                'login' => 'login',
+                'password' => 'pass',
+            ]
+        );
+
+        $this->assertEquals('yc', $result['login']);
+        $this->assertTrue(strlen($result['password']) > 5);
+    }
+}
---
+++ Tests/Helper/TextTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\RecommendationBundle\Tests\Helper;
+
+use EzSystems\RecommendationBundle\Helper\Text;
+use PHPUnit_Framework_TestCase;
+
+class TextTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider stringLists
+     */
+    public function testGetIdListFromString($input, $expected)
+    {
+        $result = Text::getIdListFromString($input);
+
+        $this->assertEquals($expected, $result);
+        $this->assertInternalType('array', $result);
+    }
+
+    public function stringLists()
+    {
+        return [
+            ['123', [123]],
+            ['123,456', [123, 456]],
+            ['12,34,56', [12, 34, 56]],
+        ];
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage String should be a list of Integers
+     */
+    public function testGetIdListFromStringWithoutSeparator()
+    {
+        Text::getIdListFromString('1abcd');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage String should be a list of Integers
+     */
+    public function testGetIdListFromStringWitInvalidArgument()
+    {
+        Text::getIdListFromString('123,abc,456');
+    }
+
+    public function getArrayFromStringDataProvider()
+    {
+        return [
+            ['123', ['123']],
+            ['123,456', ['123', '456']],
+            ['12,34,56', ['12', '34', '56']],
+            ['ab', ['ab']],
+            ['ab,bc', ['ab', 'bc']],
+        ];
+    }
+
+    /**
+     * @dataProvider getArrayFromStringDataProvider
+     */
+    public function testGetArrayFromString($input, $expected)
+    {
+        $result = Text::getArrayFromString($input);
+
+        $this->assertEquals($expected, $result);
+        $this->assertInternalType('array', $result);
+    }
+}
